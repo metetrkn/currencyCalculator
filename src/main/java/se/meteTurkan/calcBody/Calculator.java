79be1d4a3095b1e01ffee80954af1a504b92ec70
Mete Turkan <metetrkn52@gmail.com>
2024-09-08 22:13:10 +0200

    some updates

--- src/main/java/se/meteTurkan/calcBody/Calculator.java
+++ src/main/java/se/meteTurkan/calcBody/Calculator.java
@@ -6,3 +6,18 @@ public class Calculator {
         System.out.println();
     }
 }
+
+/**
+ *ISO 4217 Three Letter Currency Codes - e.g. USD for US Dollars, EUR for Euro etc
+ *
+ * // To get all information
+ * "GET https://v6.exchangerate-api.com/v6/17ac7d3a83e814c245ef9ea9/latest/USD";
+ *
+ * String currency;
+ * "GET https://v6.exchangerate-api.com/v6/17ac7d3a83e814c245ef9ea9/latest/" + currency;
+ *
+ * // Pair conversation
+ * "GET https://v6.exchangerate-api.com/v6/17ac7d3a83e814c245ef9ea9/pair/EUR/GBP";
+ * String currency2;
+ * "GET https://v6.exchangerate-api.com/v6/17ac7d3a83e814c245ef9ea9/pair/" + currency1 + currency2"
+ */
