41253c3ac0e1fdb743540553585161174c787a73
Mete Turkan <metetrkn52@gmail.com>
2024-09-08 23:15:42 +0200

    User welcoming and user preferenced currencies input classes added

--- src/main/java/se/meteTurkan/calcBody/Calculator.java
+++ src/main/java/se/meteTurkan/calcBody/Calculator.java
@@ -1,11 +1,69 @@
 package se.meteTurkan.calcBody;
-import java.math.BigDecimal;
+import java.util.Scanner; // Allows user to input
+
+
+
+// User welcoming
+class Welcomer {
+    static void welcoming() {
+        String welcomeMessage =
+            "**********************************************************************\n" +
+            "**********************************************************************\n" +
+            "*                                                                    *\n" +
+            "*  ~~~   Welcome to the Currency Converter Application v1.0.1  ~~~   *\n" +
+            "*                                                                    *\n" +
+            "**********************************************************************\n" +
+            "**********************************************************************\n\n" +
+            "The application utilizes ISO 4217 three-letter currency codes, such as USD for US Dollars and EUR for Euro.\n" +
+            "Check this web site to find out your currency unidcode:\n" +
+                "https://docs.1010data.com/1010dataReferenceManual/DataTypesAndFormats/currencyUnitCodes.html";
+
+        System.out.println(welcomeMessage);
+    }
+}
+
+
+// User currencies input
+class InputCurrency {
+    static void userInputs() {
+        Scanner myScanner = new Scanner(System.in); // Scanner object to get input
+
+        boolean key = true; // Key to check if user satisfied with input
+        do {
+            // Base currency
+            System.out.print("Please input the base currency\t:");
+            String curBase = myScanner.nextLine().toUpperCase();; // User inputSystem.out.print("Please input the base currency\t:");
+
+            // Target currency
+            System.out.print("Please input the target currency\t:");
+            String curTarget = myScanner.nextLine().toUpperCase();; // User input
+
+            // User input feedbacks
+            System.out.println("You have inputed \"" + curBase + "\" as base currency and \"" + curTarget + "\" as target currency");
+            System.out.println("Are you satisfied with currencies choose? \n1- (To accept)\t0- (To Retry)");
+
+            // User input for satisfaction with currencies
+            String userChose = myScanner.nextLine();
+
+            // Ending loop based on user preference
+            if (userChose.equals("1")) {
+                key = false;
+            }
+
+            System.out.println("\n\n"); // Leaving 2 lines empty for next messages
+
+        } while(key);
+    }
+}
+
 
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println();
+        Welcomer.welcoming(); // Prints out welcoming message
+        InputCurrency.userInputs(); // Captures user-selected currencies for comparison
     }
 }
+
 
 /**
  *ISO 4217 Three Letter Currency Codes - e.g. USD for US Dollars, EUR for Euro etc
